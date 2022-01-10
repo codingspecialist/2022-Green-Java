8aa0c6fd6ba9139705abbc09dfb8a9f905e62345
ssar <ssar@nate.com>
2022-01-10 14:41:01 +0900

    while 예제중

---
+++ src/ex06/ConEx02.java
@@ -0,0 +1,13 @@
+package ex06;
+
+public class ConEx02 {
+    public static void main(String[] args) {
+        int x = 25;
+
+        if (x < 30) {
+            System.out.println("X는 30보다 작습니다");
+        } else {
+            System.out.println("X는 30보다 큽니다");
+        }
+    }
+}
